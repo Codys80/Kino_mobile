7e45c3ad171fb00a87ea7f3223b16118c7a0f260
CodyS80 <bartolosemolo@gmail.com>
2024-10-31 08:27:11 +0100

    Dodano projekt

---
+++ MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace Kino
+{
+    public partial class MainPage : ContentPage
+    {
+        double cena = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Buy(object sender, EventArgs e)
+        {
+            cena += 15;
+                koszyk.Text = $"Koszyk: {cena} zł";
+        }
+    }
+
+}
